b813d9f1fdf4b5b9c1877d58b07c3d5f9c9f39cd
xyzxxz <xuyinzhuge@qq.com>
2023-01-14 16:07:27 +0800

    basic structure

---
+++ init.el
@@ -0,0 +1,11 @@
+; emacs ELPA 中科大源
+(setq package-archives '(("gnu" . "http://mirrors.ustc.edu.cn/elpa/gnu/")
+                         ("melpa" . "http://mirrors.ustc.edu.cn/elpa/melpa/")
+                         ("nongnu" . "http://mirrors.ustc.edu.cn/elpa/nongnu/")))
+
+(add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
+
+(require 'init-ui)
+(require 'init-theme)
+
+(provide 'init)
