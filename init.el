fabe7e52ce95441491a32a45dfefa0aa75621ea7
xyzxxz <xuyinzhuge@qq.com>
2023-02-01 16:09:28 +0800

    change

--- init.el
+++ init.el
@@ -6,6 +6,6 @@
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
 
 (require 'init-ui)
-(require 'init-theme)
+;(require 'init-theme)
 
 (provide 'init)
--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -1,2 +1,6 @@
-
+; 字体
+(add-to-list 'default-frame-alist
+             '(font . "Hack-13"))
+; 禁用滚动条
+(scroll-bar-mode nil)
 (provide 'init-ui)
