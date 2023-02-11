9f35714081493acd3b4cc222b7ba41799469a03f
xyzxxz <xuyinzhuge@qq.com>
2023-02-11 17:07:36 +0800

    add evil

--- init.el
+++ init.el
@@ -1,3 +1,5 @@
+(require 'package)
+
 ; emacs ELPA 中科大源
 (setq package-archives '(("gnu" . "http://mirrors.ustc.edu.cn/elpa/gnu/")
                          ("melpa" . "http://mirrors.ustc.edu.cn/elpa/melpa/")
@@ -12,6 +14,20 @@
 ; 关闭自动保存
 (setq auto-save-default nil)
 
+(require 'init-evil)
 (require 'init-ui)
 
 (provide 'init)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(evil))
+ '(warning-suppress-log-types '((comp))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
